37f3be58d6ec58b614229f26e88a8759f23be779
Lex Southin-webb <lexwebb@gmail.com>
2021-04-10 14:01:48 +0100

    Export new elements

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ import Button from "./Button";
 import Checkbox from "./Checkbox";
 import Select from "./Select";
 import Combobox from "./Combobox";
+import Typography from "./Typography";
+import Card from "./Card";
 
-export { Button, Checkbox, Select, Combobox };
+export { Button, Checkbox, Select, Combobox, Typography, Card };
