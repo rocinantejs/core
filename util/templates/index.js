18e48728651cb5c95b8f750f2415ef27c102393a
Lex Southin-webb <lexwebb@gmail.com>
2021-04-18 21:18:13 +0100

    Fix lint and template

--- util/templates/index.js
+++ util/templates/index.js
@@ -1,6 +1,7 @@
 module.exports = (componentName) => ({
   content: `// Generated with util/create-component.js
-export { default } from "./${componentName}";`,
+export { default } from "./${componentName}";
+`,
   extension: `.ts`,
   overrideName: `index`,
 });
